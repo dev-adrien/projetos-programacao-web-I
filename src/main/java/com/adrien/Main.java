055ec94b0b4cc03a12b6eb5bce3298fa85020ae9
adrien <adrienbzr@outlook.com>
2026-08-02 15:38:50 -0300

    :tada: init: cria a estrutura inicial do projeto

---
+++ src/main/java/com/adrien/Main.java
@@ -0,0 +1,13 @@
+package com.adrien;
+
+import java.util.Optional;
+
+import com.adrien.dao.InstrutorDAO;
+import com.adrien.db.DBFactory;
+import com.adrien.entities.Instrutor;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
