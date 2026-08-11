6f62a5acaf3b134bf056a9c0b28c96580a9ae309
adrien <adrienbzr@outlook.com>
2026-08-11 19:34:27 -0300

    :sparkles: feat: implementa a interface InstrutorRepository

---
+++ src/main/java/com/adrien/parte4spring/repositories/InstrutorRepository.java
@@ -0,0 +1,11 @@
+package com.adrien.parte4spring.repositories;
+
+
+import com.adrien.parte4spring.entities.Instrutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface InstrutorRepository extends JpaRepository<Instrutor, Long> {
+
+}
+
