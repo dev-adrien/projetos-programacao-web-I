12a6369dd468d30f612b2a2984a0c0003d84a324
adrien <adrienbzr@outlook.com>
2026-08-11 17:40:30 -0300

    :tada: init: cria estrutura inicial do projeto

---
+++ src/main/java/com/adrien/parte4spring/Parte4springApplication.java
@@ -0,0 +1,13 @@
+package com.adrien.parte4spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parte4springApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Parte4springApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adrien/parte4spring/Parte4springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrien.parte4spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Parte4springApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
